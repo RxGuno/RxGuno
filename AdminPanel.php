fd065376b5964a3908971a42c3b1d542059f522e
toybets <163895963+toybets@users.noreply.github.com>
2024-03-30 18:54:14 +0800

    AdminPanel.php

--- AdminPanel.php
+++ AdminPanel.php
@@ -14,7 +14,7 @@ if (isset ($_SESSION ['id'] ) && isset ($_SESSION ['empid'])){
 
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="adminpanel.css">
+  <link rel="stylesheet" type="text/css" href="admin panel.css">
   <title style="font-size: large;">Homepage</title>
 
 
@@ -25,7 +25,7 @@ if (isset ($_SESSION ['id'] ) && isset ($_SESSION ['empid'])){
   <a href="#Home">Customers Account</a>
   <a href="AdminACC.php">Admin Account</a>
   <a href="ProductDB.php">Product</a>
-  <a href="#About">Order History</a>
+  <a href="#Order">Order History</a>
 
         <div class="logout">
         <a style="text-decoration:none" href= "logout.php">Logout</a>
@@ -38,19 +38,51 @@ if (isset ($_SESSION ['id'] ) && isset ($_SESSION ['empid'])){
   <div id="mySidenav">
     <a href="#" class="closebtn">&times;</a>
     <a href="#Home">Home</a>
-    <a href="#Video">Video</a>
-    <a href="gallery.html">Gallery</a>
-    <a href="#About">Location</a>
+    <a href="#AdminACC.php">Admin</a>
+    <a href="ProductDB.php">Product</a>
+    <a href="#Order">Order History</a>
     <div class="logout">
         <a style="text-decoration:none" href= "logout.php">Logout</a>
         </div>
   </div>
 
 <div id="Home">
-<div class="homecorn">
-<img src="homecorn.jpg">
+<div class="logohome">
+<img src="AA Main Logo.png">
 </div>
 </div>
+
+<div class="box">
+<article>
+    <a href="#">Customer Account</a>
+        <img src="person-circle-outline.svg" class="icon">
+</article>
+</div>
+
+
+<div class="box">
+    <article>
+        <a href="#">Admin Account</a>
+            <img src="person-circle-sharp.svg" class="icon">
+    </article>
+    </div>
+    
+
+
+    <div class="box">
+        <article>
+            <a href="#">Product</a>
+                <img src="shirt-sharp.svg" class="icon">
+        </article>
+        </div>
+
+
+        <div class="box">
+            <article>
+                <a href="#">Order history</a>
+                    <img src="receipt-sharp.svg" class="icon">
+            </article>
+            </div>
 
 
 </body>
